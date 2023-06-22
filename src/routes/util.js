672eb519eeeca80e71454c0aef9a23121db6104c
Haki-D-Rey <maleisho@gmail.com>
2023-06-22 14:29:38 -0600

    fix: veridy token user admin upload image

--- src/routes/util.js
+++ src/routes/util.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
-const { verifyToken } = require("../helper/middleware/verifyToken");
+const {
+  verifyToken,
+  verifyTokenAutorizationAdminUser,
+} = require("../helper/middleware/verifyToken");
 
 const { uploadFile } = require("../helper/utils/S3");
 
@@ -7,13 +10,17 @@ router.get("/", verifyToken, (req, res) => {
   res.json({ message: "get images" });
 });
 
-router.post("/image/upload", async (req, res) => {
-  const result = await uploadFile(req.files["photo"]);
-  console.log(result);
-  if (!result.fieldError) {
-    return res.status(201).json({ url: result.url, message: "upload image" });
+router.post(
+  "/image/upload",
+  verifyTokenAutorizationAdminUser,
+  async (req, res) => {
+    const result = await uploadFile(req.files["photo"]);
+    console.log(result);
+    if (!result.fieldError) {
+      return res.status(201).json({ url: result.url, message: "upload image" });
+    }
+    return res.status(400).json({ message: "Error upload image" });
   }
-  return res.status(400).json({ message: "Error upload image" });
-});
+);
 
 module.exports = router;
